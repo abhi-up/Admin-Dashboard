5270772e89ce0990290372069a3b150e8614e6e7
Abhi <meabhishek1512@gmail.com>
2024-04-18 18:46:04 +0530

    footer design

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,12 @@
 import "./footer.scss"
+
 const Footer = () => {
-    return <div className="footer">Footer</div>
+    return (
+        <div className="footer">
+            <span>AlphaAdmin</span>
+            <span>© Abhi's Admin Dashboard</span>
+        </div>
+    )
 }
 
 export default Footer
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div className="navbar">
             <div className="logo">
                 <img src="logo.svg" alt="" />
-                <span>Admin Dashboard</span>
+                <span>AlphaAdmin</span>
             </div>
             <div className="icons">
                 <img src="/search.svg" alt="" className="icon" />
