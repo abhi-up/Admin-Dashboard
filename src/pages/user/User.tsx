a0bec27f3c05514a5def48db23052f5889822136
Abhi <meabhishek1512@gmail.com>
2024-04-20 14:21:38 +0530

    updated: user component

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,6 +1,5 @@
 import Single from "../../components/single/Single"
 import { singleUser } from "../../data"
-import "./user.scss"
 
 const User = () => {
     //Fetch data and send to Single Component
