9065f991d624ecc9c7245a011ab50394ddbcae7f
Abhi <meabhishek1512@gmail.com>
2024-04-20 14:17:02 +0530

    single product component created

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,5 +1,13 @@
+import Single from "../../components/single/Single"
+import { singleProduct } from "../../data"
+
 const Product = () => {
-    return <div>Product</div>
+    //Fetch data and send to Single Component
+    return (
+        <div className="product">
+            <Single {...singleProduct} />
+        </div>
+    )
 }
 
 export default Product
