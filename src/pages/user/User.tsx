9523dadeb709b1fbdc3d2415eed57d5183a0f68a
Abhi <meabhishek1512@gmail.com>
2024-04-20 14:17:36 +0530

    single user component created

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,5 +1,15 @@
+import Single from "../../components/single/Single"
+import { singleUser } from "../../data"
+import "./user.scss"
+
 const User = () => {
-    return <div>User</div>
+    //Fetch data and send to Single Component
+
+    return (
+        <div className="user">
+            <Single {...singleUser} />
+        </div>
+    )
 }
 
 export default User
